a22d4c5e61f9dd4bebc2c4d35dd15d35dc6a00d4
Zahid71187 <zahidyramirezgonzalez@gmail.com>
2025-06-12 00:39:24 -0600

    Update zona AWS

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const log = require('./commons/logger')
 app.use(express.json());
 app.use('/api/v1', prestamosRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Bienvenido a la API de Prestamos');
+}
+);
+
 const PORT = process.env.PORT || 8080;
 
 MongoConection.createConnection().then(() => {
